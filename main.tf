b08729232f64111e290d8daf74db421e20cb32a9
Piotr Koska <piotr.s.koska@gmail.com>
2024-10-31 15:02:54 +0100

    dodanie vpc i vm

--- main.tf
+++ main.tf
@@ -3,3 +3,17 @@ resource "digitalocean_project" "main" {
   description = "My project description"
   environment = "development"
 }
+
+resource "digitaocean_vpc" "main" {
+    name = "my-vpc"
+    region = "fra1"
+    description = "My VPC"
+}
+
+resource "digitalocean_droplet" "main" {
+  name  = "my-droplet"
+  image  = "ubuntu-20-04-x64"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  vpc_uuid = digitalocean_vpc.main.id
+}
